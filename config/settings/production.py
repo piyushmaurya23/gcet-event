6fe931f88dda51fe8af11b4ae75234b3748cc0e8
Piyush Maurya <piyush.maurya@live.in>
2016-11-29 20:56:00 +0530

    ALLOWED_HOSTS

--- config/settings/production.py
+++ config/settings/production.py
@@ -67,7 +67,8 @@ X_FRAME_OPTIONS = 'DENY'
 # ------------------------------------------------------------------------------
 # Hosts/domain names that are valid for this site
 # See https://docs.djangoproject.com/en/1.6/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['139.59.46.140',])
+# ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['139.59.46.140'])
+ALLOWED_HOSTS = ['139.59.12.176']
 # END SITE CONFIGURATION
 
 INSTALLED_APPS += ('gunicorn', )
